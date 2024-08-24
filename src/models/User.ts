a72e3efe8df528e6dfa35474b22b6cf1bb220814
KakatkarAkshay <akshayakakatkaraa@gmail.com>
2024-08-24 11:19:01 +0000

    Define user model

---
+++ src/models/User.ts
@@ -0,0 +1,43 @@
+import { type Document, Schema, model } from "mongoose";
+import type { ObjectId } from "../types";
+
+export enum UserRole {
+	ADMIN = "admin",
+	MEMBER = "member",
+}
+
+export interface IUser {
+	email: string;
+	emailVerified: boolean;
+	role: UserRole;
+}
+
+export interface IUserDocument extends IUser, Document<ObjectId> {}
+
+const UserSchema = new Schema<IUserDocument>(
+	{
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+			trim: true,
+		},
+		emailVerified: {
+			type: Boolean,
+			default: false,
+		},
+		role: {
+			type: String,
+			enum: Object.values(UserRole),
+			default: UserRole.MEMBER,
+			required: true,
+		},
+	},
+	{
+		timestamps: true,
+	},
+);
+
+const User = model<IUserDocument>("User", UserSchema);
+
+export default User;
---
+++ src/types/index.ts
@@ -0,0 +1,4 @@
+import { Types } from "mongoose";
+
+export const ObjectId = Types.ObjectId;
+export type ObjectId = Types.ObjectId;
