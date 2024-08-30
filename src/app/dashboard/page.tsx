f813bfa106828813fc36237b8424d69278c0ac79
KakatkarAkshay <akshayakakatkaraa@gmail.com>
2024-08-30 05:00:49 +0000

    Force dynamic

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,8 @@
 import Dashboard from "@/components/dashboard/Dashboard";
 import Guard from "@/components/guards/Guard";
 
+export const dynamic = "force-dynamic";
+
 export default function DashboardPage() {
   return (
     <Guard>
